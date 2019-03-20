35a1bb51cda9a4958877514c1b0329ea87010339
Karthik R <rkarthik42@gmail.com>
2019-03-21 00:20:28 +0530

    Update testDownloadSpeed.sh

--- testDownloadSpeed.sh
+++ testDownloadSpeed.sh
@@ -4,17 +4,18 @@
 # One link per line with location and host.
 # link<space>location<space>host
 
-date
+date | tee -a $HOME/bench.log
 
 infile=$1
 i=1
 while read -r line
 do
-    printf "Line %3d: %s\n" $i "$line"
+    printf "Line %3d: %s\n" $i "$line" | tee -a $HOME/bench.log
     arr=($line)
         node_server=$( wget -4 -O /dev/null ${arr[0]} 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-        printf "${arr[1]} \t ${arr[2]} \t $node_server " | tee -a $HOME/bench.log
+        printf "Avg speed: $node_server " | tee -a $HOME/bench.log
         echo "" | tee -a $HOME/bench.log
     let i=i+1
 done<$infile
+
 
